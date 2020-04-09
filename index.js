97e89243c2819864b2fae5eb3e9b195a24129c4a
AkaEthan110 <ethanhorrocks06@outlook.com>
2020-04-09 13:28:57 +0100

    Update

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const client = new Discord.Client({ disableEveryone: true });
 const createCaptcha = require('./captcha');
 const fs = require('fs').promises;
 
-const prefix = ""; // The bot's prefix.
-
-const emoji = ""; // The emoji that the user has to press to verify themself.
-const message = ""; // The message which the bot will send in the verification channel.
+const prefix = "-"; // The bot's prefix.
+const emoji = "✅"; // The emoji that the user has to press to verify themself.
+const message = "Hello! :slight_smile:"; // The message which the bot will send in the verification channel.
 
 client.on("message", async message => {
     if(user === bot.user) return;
@@ -28,10 +27,10 @@ client.on("messageReactionAdd", (messageReaction, user) => {
     
     if(emoji.name === emoji) {
         if(message.channel.id === verificationChannel) {
-            const roleToAdd = message.guild.roles.get(""); // The role the bot will add to the user when they complete verification.
-            const roleToRemove = message.guild.roles.get(""); // The role the bot will remove from the user when they complete verification.
-            const verificationChannel = message.guild.channels.get(""); // The verification channel.
-            const logs = message.guild.channels.get(""); // The logs channel.
+            const roleToAdd = message.guild.roles.get("697784771144712283"); // The role the bot will add to the user when they complete verification.
+            const roleToRemove = message.guild.roles.get("697784789263974452"); // The role the bot will remove from the user when they complete verification.
+            const verificationChannel = message.guild.channels.get("697785007137095750"); // The verification channel.
+            const logs = message.guild.channels.get("697783934653825075"); // The logs channel.
 
             message.channel.send(":white_check_mark:").then(msg => { msg.delete(3000) });
             user.send(`:white_check_mark: You have successfully verified youself in the server: \`${message.guild.name}\`!`)
