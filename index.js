79341766e9e30e9ce3c9495d9d788eb4169ed3cf
AkaEthan110 <ethanhorrocks06@outlook.com>
2020-04-09 13:01:07 +0100

    .

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const createCaptcha = require('./captcha');
 const fs = require('fs').promises;
 
 client.on('guildMemberAdd', async member => {
+    if(member.bot) return;
+
     console.log("1")
 
     const time = 120; // How many seconds the user has to solve the captcha.
@@ -14,7 +16,7 @@ client.on('guildMemberAdd', async member => {
 
     console.log("2")
 
-    logs.send(`${member.tag} has joined the server and has ${time} seconds to solve the captcha which I sent to their DMs!`);
+    logs.send(`${member.user.tag} has joined the server and has ${time} seconds to solve the captcha which I sent to their DMs!`);
 
     console.log("3")
 
@@ -32,14 +34,14 @@ client.on('guildMemberAdd', async member => {
                 if(m.author.id === member.id && m.content === captcha) return true;
                 else {
                     m.channel.send(':x: You entered the captcha incorrectly!');
-                    logs.send(`:x: ${member.tag} entered their captcha incorrectly!`);
+                    logs.send(`:x: ${member.user.tag} entered their captcha incorrectly!`);
                     return false;
                 }
             };
             const response = await msg.channel.awaitMessages(filter, { max: 1, time: `${time}`*1000, errors: ['time']});
             if(response) {
                 await msg.channel.send(':white_check_mark: You have successfully verified yourself!');
-                logs.send(`:white_check_mark: ${member.tag} has successfully completed the captcha and has proven that they are not a robot!`)
+                logs.send(`:white_check_mark: ${member.user.tag} has successfully completed the captcha and has proven that they are not a robot!`)
                 //await member.roles.add(role);
                 //await member.roles.remove(role2);
                 await fs.unlink(`${__dirname}/captchas/${captcha}.png`)
@@ -49,7 +51,7 @@ client.on('guildMemberAdd', async member => {
         catch(err) {
             console.log(err);
             await msg.channel.send(':x: You didn\'t solve the captcha correctly on time! Please rejoin the server and try again!');
-            logs.send(`:x: ${member.tag} didn't solve the captcha correctly on time! I have kicked them from the server and told them to rejoin and try the captcha again.`)
+            logs.send(`:x: ${member.user.tag} didn't solve the captcha correctly on time! I have kicked them from the server and told them to rejoin and try the captcha again.`)
             await member.kick();
             await fs.unlink(`${__dirname}/captchas/${captcha}.png`)
                     .catch(err => console.log(err));
