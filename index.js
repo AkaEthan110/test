b5e662bf93af14156e075cddd5884b164b2778e7
AkaEthan110 <ethanhorrocks06@outlook.com>
2020-04-09 13:25:04 +0100

    New

--- index.js
+++ index.js
@@ -4,62 +4,42 @@ const client = new Discord.Client({ disableEveryone: true });
 const createCaptcha = require('./captcha');
 const fs = require('fs').promises;
 
-client.on('guildMemberAdd', async member => {
-    if(member.bot) return;
+const prefix = ""; // The bot's prefix.
 
-    console.log("1")
+const emoji = ""; // The emoji that the user has to press to verify themself.
+const verificationChannel = message.guild.channels.get(""); // The verification channel.
+const logs = message.guild.channels.get(""); // The logs channel.
+const message = ""; // The message which the bot will send in the verification channel.
 
-    const time = 120; // How many seconds the user has to solve the captcha.
-    const logs = member.guild.channels.get("697557759868272841"); // The channel ID of where all logs are sent.
-    const role = member.guild.roles.get(""); // The role ID that is given when a user completes the captcha.
-    const role2 = member.guild.roles.get(""); // The role ID that is removed when a user completes the captcha.
+client.on("message", async message => {
+    if(user === bot.user) return;
 
-    console.log("2")
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
 
-    logs.send(`${member.user.tag} has joined the server and has ${time} seconds to solve the captcha which I sent to their DMs!`);
-
-    console.log("3")
-
-    const captcha = await createCaptcha();
-    try {
-        const msg = await member.send(`:stopwatch: You have ${time} seconds to solve this captcha:`, {
-            files: [{
-                attachment: `${__dirname}/captchas/${captcha}.png`,
-                name: `${captcha}.png`
-            }]
+    if(command === "sendMessage") {
+        message.channel.send(message).then(message => {
+            message.react(emoji);
         });
-        try {
-            const filter = m => {
-                if(m.author.bot) return;
-                if(m.author.id === member.id && m.content === captcha) return true;
-                else {
-                    m.channel.send(':x: You entered the captcha incorrectly!');
-                    logs.send(`:x: ${member.user.tag} entered their captcha incorrectly!`);
-                    return false;
-                }
-            };
-            const response = await msg.channel.awaitMessages(filter, { max: 1, time: `${time}`*1000, errors: ['time']});
-            if(response) {
-                await msg.channel.send(':white_check_mark: You have successfully verified yourself!');
-                logs.send(`:white_check_mark: ${member.user.tag} has successfully completed the captcha and has proven that they are not a robot!`)
-                //await member.roles.add(role);
-                //await member.roles.remove(role2);
-                await fs.unlink(`${__dirname}/captchas/${captcha}.png`)
-                    .catch(err => console.log(err));
-            }
-        }
-        catch(err) {
-            console.log(err);
-            await msg.channel.send(':x: You didn\'t solve the captcha correctly on time! Please rejoin the server and try again!');
-            logs.send(`:x: ${member.user.tag} didn't solve the captcha correctly on time! I have kicked them from the server and told them to rejoin and try the captcha again.`)
-            await member.kick();
-            await fs.unlink(`${__dirname}/captchas/${captcha}.png`)
-                    .catch(err => console.log(err));
-        }
     }
-    catch(err) {
-        console.log(err);
-    }
+});
+
+client.on("messageReactionAdd", (messageReaction, user) => {
+    if(user.bot) return;
+    const { message, emoji } = messageReaction;
+    
+    if(emoji.name === emoji) {
+        if(message.channel.id === verificationChannel) {
+            const roleToAdd = message.guild.roles.get(""); // The role the bot will add to the user when they complete verification.
+            const roleToRemove = message.guild.roles.get(""); // The role the bot will remove from the user when they complete verification.
+
+            message.channel.send(":white_check_mark:").then(msg => { msg.delete(3000) });
+            user.send(`:white_check_mark: You have successfully verified youself in the server: \`${message.guild.name}\`!`)
+            user.addRole(roleToAdd.id);
+            user.removeRole(roleToRemove.id);
+            logs.send(`${user.tag} has successfully verified themself!`)
+        }
+    } 
 });
 
 client.login("NjgwMDAwODY3OTgxOTgzNzgz.Xo50vw.xqSTW8Qi37UTVDl0jT9nUCZb654");
