bc30444b2f3db61730b04f8b1cbd503c25ef9651
AkaEthan110 <ethanhorrocks06@outlook.com>
2020-04-09 13:43:58 +0100

    .

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ client.on("messageReactionAdd", (messageReaction, user) => {
 
         const verificationChannel = message.guild.channels.get("697785007137095750"); // The verification channel.
 
-        if(message.channel.id === verificationChannel) {
+        if(message.channel.id == verificationChannel) {
             console.log(!"3")
 
             const roleToAdd = message.guild.roles.get("697784771144712283"); // The role the bot will add to the user when they complete verification.
